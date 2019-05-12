17477b7bf538842c1e88de7ca47934d4cd2ee515
Glitch (google-pwa) <none>
2019-05-12 17:50:24 +0000

    Log user response to prompt

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -49,7 +49,15 @@ function installPWA(evt) {
   evt.srcElement.setAttribute('hidden', true);
 
   // CODELAB: Log user response to prompt.
-
+  deferredInstallPrompt.userChoice
+      .then((choice) => {
+        if (choice.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt', choice);
+        } else {
+          console.log('User dismissed the A2HS prompt', choice);
+        }
+        deferredInstallPrompt = null;
+      });
 }
 
 // CODELAB: Add event listener for appinstalled event
