b0ea0ce0f3872f3c9bf7cb96ab039442012ad2e9
Glitch (google-pwa) <none>
2019-05-12 17:51:48 +0000

    Add code to log the event

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -71,5 +71,5 @@ window.addEventListener('appinstalled', logAppInstalled);
  */
 function logAppInstalled(evt) {
   // CODELAB: Add code to log the event
-
+  console.log('Weather App was installed.', evt);
 }
