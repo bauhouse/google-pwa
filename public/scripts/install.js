b4eb680cf0431d763ed5cb981a9932b6996c66ef
Glitch (google-pwa) <none>
2019-05-12 17:50:55 +0000

    Add event listener for appinstalled event

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -61,6 +61,7 @@ function installPWA(evt) {
 }
 
 // CODELAB: Add event listener for appinstalled event
+window.addEventListener('appinstalled', logAppInstalled);
 
 /**
  * Event handler for appinstalled event.
